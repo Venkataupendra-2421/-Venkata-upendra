cc83fe1152c6e29b7c09d076d771c8ec06cae5d4
Venkataupendra-2421 <112751688+Venkataupendra-2421@users.noreply.github.com>
2022-09-07 10:01:16 +0530

    Add files via upload

---
+++ half pyramid.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int rows,i,j;
+	cout<<"Enter number of rows : ";
+	cin>>rows;
+	
+	for(i=1;i<=rows;i++)
+	{
+		for(j=1;j<=i;++j)
+		{
+			cout<<j<<" ";
+			
+		}
+		cout<<"\n";
+		
+	}
+	return 0;
+}
